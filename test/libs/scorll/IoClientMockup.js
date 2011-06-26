49e909540aba6320c2d22e214c1c62dc2ed1010a
Cagdas Tulek <ctulek@gmail.com>
2011-06-26 00:54:41 -0700

    IoClientMockup: Use the new socket.io classes

--- test/libs/scorll/IoClientMockup.js
+++ test/libs/scorll/IoClientMockup.js
@@ -4,6 +4,12 @@ var events = require('events');
 var IoClientMockup = function (sessionId) {
         this.sessionId = sessionId;
         this.lastMessage = null;
+        var mockup = this;
+        this.json = {
+            send: function (message) {
+                mockup.lastMessage = message;
+            }
+        }
     }
 
 util.inherits(IoClientMockup, events.EventEmitter);
@@ -11,10 +17,6 @@ util.inherits(IoClientMockup, events.EventEmitter);
 IoClientMockup.getInstance = function (sessionId) {
     var ioClientMockup = new IoClientMockup(sessionId);
     return ioClientMockup;
-}
-
-IoClientMockup.prototype.send = function (message) {
-    this.lastMessage = message;
 }
 
 module.exports = IoClientMockup;
