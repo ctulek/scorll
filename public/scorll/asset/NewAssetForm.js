a6b870365992b0de502c08e1b8caeb6b5897a010
Cagdas Tulek <ctulek@gmail.com>
2011-07-24 20:36:37 -0700

    NewAssetForm: Make asset selection part a form

---
+++ public/scorll/asset/NewAssetForm.js
@@ -0,0 +1,47 @@
+dojo.provide("scorll.asset.NewAssetForm");
+
+dojo.require("dijit.TitlePane");
+dojo.require("dijit.form.Button");
+dojo.require("scorll.asset.AssetManager");
+
+dojo.declare("scorll.asset.NewAssetForm", [dijit._Widget, dijit._Templated], {
+  widgetsInTemplate: true,
+  templatePath: dojo.moduleUrl("scorll.asset", "NewAssetForm.html"),
+  postCreate: function() {
+    var newAssetForm = this;
+    var stage = this.stage;
+    var container = this.container;
+    dojo.destroy(container.arrowNode);
+    container.startup();
+    var assetList = this.assetList;
+    var assetManager = new scorll.asset.AssetManager();
+    for (var type in assetManager.assets) {
+      var button = new dijit.form.Button();
+      var label = assetManager.assets[type].label;
+      button.attr('label', label);
+      button.widgetType = type;
+      dojo.connect(button, "onClick", function() {
+        container.attr('title', this.attr('label'));
+        var form = assetManager.getAssetForm({
+          type: this.widgetType
+        });
+        dojo.place(form.domNode, container.containerNode, "only");
+        window.scrollTo(0, document.body.scrollHeight);
+        dojo.connect(form, "onSubmit", function(item) {
+          stage.content.add(item);
+          newAssetForm.onSubmit();
+        });
+        dojo.connect(form, "onCancel", function() {
+          newAssetForm.cancel();
+        });
+      });
+      button.placeAt(assetList);
+      container.containerNode.scrollIntoView()
+    }
+  },
+  onSubmit: function() {},
+  onCancel: function() {},
+  cancel: function() {
+    this.onCancel();
+  }
+});
